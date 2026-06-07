c5b932a9fffec5a7e61a49c428ed3b488a53aa7d
242282218 <242282218@users.noreply.github.com>
2026-06-07 13:57:19 +0800

    chore(android): bump version to 0.25.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = "com.aichat.workbench"
         minSdk = 26
         targetSdk = 36
-        versionCode = 25
-        versionName = "0.24.0"
+        versionCode = 26
+        versionName = "0.25.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
